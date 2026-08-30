e87002f15b786cce4c8d5eb9bd4587d099cd7df9
AlexThunder2014 <alexander@maass.de>
2026-08-30 18:40:04 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,4 @@
-// ============================================
-// DETEKTIV-JAGD
-// FIREBASE VERBINDUNG
-// ============================================
-
-import { initializeApp }
-    from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
 
 import {
     getDatabase,
@@ -17,88 +11,30 @@ import {
     push
 } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-database.js";
 
-
-// ============================================
-// FIREBASE KONFIGURATION
-// ============================================
-
 const firebaseConfig = {
-
-    apiKey:
-        "AIzaSyACSnquBUpWqgUy9ck5nrICTCQZfq6pbpM",
-
-    authDomain:
-        "dedektive-66ed9.firebaseapp.com",
-
-    projectId:
-        "dedektive-66ed9",
-
-    storageBucket:
-        "dedektive-66ed9.firebasestorage.app",
-
-    messagingSenderId:
-        "1033107065836",
-    
-    databaseURL:
-        "https://dedektive-66ed9-default-rtdb.europe-west1.firebasedatabase.app",
-    
-    appId:
-        "1:1033107065836:web:afee087737bfe62c6dcec9",
-
-    databaseURL:
-        "https://dedektive-66ed9-default-rtdb.europe-west1.firebasedatabase.app/"
-
+    apiKey: "AIzaSyACSnquBUpWqgUy9ck5nrICTCQZfq6pbpM",
+    authDomain: "dedektive-66ed9.firebaseapp.com",
+    projectId: "dedektive-66ed9",
+    storageBucket: "dedektive-66ed9.firebasestorage.app",
+    messagingSenderId: "1033107065836",
+    appId: "1:1033107065836:web:afee087737bfe62c6dcec9",
+    databaseURL: "https://dedektive-66ed9-default-rtdb.europe-west1.firebasedatabase.app"
 };
 
+const app = initializeApp(firebaseConfig);
 
-// ============================================
-// FIREBASE STARTEN
-// ============================================
-
-const app =
-    initializeApp(firebaseConfig);
-
-
-// ============================================
-// REALTIME DATABASE STARTEN
-// ============================================
-
-const database =
-    getDatabase(app);
-
-
-// ============================================
-// FÜR APP.JS UND ADMIN.JS BEREITSTELLEN
-// ============================================
+const database = getDatabase(app);
 
 window.DetektivDB = {
-
-    app: app,
-
-    database: database,
-
-    ref: ref,
-
-    set: set,
-
-    get: get,
-
-    update: update,
-
-    remove: remove,
-
-    onValue: onValue,
-
-    push: push
-
+    app,
+    database,
+    ref,
+    set,
+    get,
+    update,
+    remove,
+    onValue,
+    push
 };
 
-
-console.log(
-    "🔥 Firebase erfolgreich verbunden!"
-);
-
-console.log(
-    "🗄️ Realtime Database:",
-    database
-);
+console.log("🔥 Firebase erfolgreich verbunden");
